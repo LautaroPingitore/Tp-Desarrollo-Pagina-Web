6c201a64cb68a43a2fb91653362deea466102b6f
Lautaro Pingitore <lpingitore@frba.utn.edu.ar>
2025-07-09 18:01:04 -0300

    :(

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import MisAlojamientos from './components/misAlojamientos/misAlojamientos';
 import Reservas from './components/reservas/reservas';
 import Notificaciones from './components/misNotificaciones/misNotificaciones';
 import CrearAlojamiento from './components/crearAlojamiento/crearAlojamiento';
-import { AuthProvider } from '../src/context/authContext'
+import { AuthProvider } from './context/authContext.jsx'
 
 const theme = createTheme({
   palette: {
