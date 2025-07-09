1a97762cd07c79fe4954d758dff2602536c200f6
Lautaro Pingitore <lpingitore@frba.utn.edu.ar>
2025-07-09 17:32:49 -0300

    Creo que ya estaria todo

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import qs from "qs";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const VITE_API = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_URL = `${VITE_API}/birbnb`;
 
 export const getAlojamientos = async (pageNumber, filtros) => {
     try {
